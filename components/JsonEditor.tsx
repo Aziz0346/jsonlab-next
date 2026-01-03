ab3ece0c7a100d6f18ac7237d440f4fa4d5f7d2d
Aziz0346 <82248616+Aziz0346@users.noreply.github.com>
2026-01-03 23:34:33 +0500

    Create JsonEditor.tsx

---
+++ components/JsonEditor.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function JsonEditor() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>JSON Lab</h1>
+      <p>
+        A free, privacy-first JSON editor that runs entirely in your browser.
+      </p>
+
+      {/* Your existing editor logic will go here */}
+    </main>
+  );
+}
